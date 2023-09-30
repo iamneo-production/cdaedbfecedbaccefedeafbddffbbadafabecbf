8aeb3646ae09159f3a5b7c26588b227ff6b1e662
Iamneo <techadmin@iamneo.ai>
2023-09-30 06:19:21 +0000

    Backup Commit

--- reactapp/src/Calculator.jsx
+++ reactapp/src/Calculator.jsx
@@ -1,102 +1,101 @@
-// Calculator.js
-
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './Calculator.css';
 
-class Calculator extends Component {
-  constructor() {
-    super();
-    this.state = {
-      displayValue: '0',
-      operator: null,
-      previousValue: null,
-    };
-  }
+function Calculator() {
+  const [displayValue, setDisplayValue] = useState('0');
+  const [firstOperand, setFirstOperand] = useState(null);
+  const [operator, setOperator] = useState(null);
+  const [waitingForSecondOperand, setWaitingForSecondOperand] = useState(false);
 
-  handleButtonClick = (value) => {
-    if (value === 'C') {
-      this.clearDisplay();
-    } else if (value === '=') {
-      this.calculateResult();
+  const handleButtonClick = (value) => {
+    if (waitingForSecondOperand) {
+      setDisplayValue(value);
+      setWaitingForSecondOperand(false);
     } else {
-      this.addToDisplay(value);
+      setDisplayValue(displayValue === '0' ? value : displayValue + value);
     }
   };
 
-  addToDisplay = (value) => {
-    const { displayValue } = this.state;
-    if (displayValue === '0' || this.state.operator) {
-      this.setState({ displayValue: value });
-    } else {
-      this.setState({ displayValue: displayValue + value });
+  const handleOperatorClick = (nextOperator) => {
+    if (firstOperand === null) {
+      setFirstOperand(displayValue);
+    } else if (operator) {
+      const result = calculate();
+      setFirstOperand(result);
+      setDisplayValue(result);
+    }
+
+    setOperator(nextOperator);
+    setWaitingForSecondOperand(true);
+  };
+
+  const calculate = () => {
+    const prev = parseFloat(firstOperand);
+    const current = parseFloat(displayValue);
+
+    switch (operator) {
+      case '+':
+        return prev + current;
+      case '-':
+        return prev - current;
+      case '*':
+        return prev * current;
+      case '/':
+        if (current === 0) return 'Error';
+        return prev / current;
+      default:
+        return current;
     }
   };
 
-  clearDisplay = () => {
-    this.setState({
-      displayValue: '0',
-      operator: null,
-      previousValue: null,
-    });
-  };
-
-  calculateResult = () => {
-    const { displayValue, operator, previousValue } = this.state;
-    if (operator && previousValue) {
-      const result = eval(`${previousValue} ${operator} ${displayValue}`);
-      this.setState({
-        displayValue: String(result),
-        operator: null,
-        previousValue: null,
-      });
+  const handleEqualClick = () => {
+    if (operator && firstOperand !== null) {
+      const result = calculate();
+      setDisplayValue(result);
+      setFirstOperand(result);
+      setOperator(null);
+      setWaitingForSecondOperand(false);
     }
   };
 
-  setOperator = (operator) => {
-    const { displayValue, previousValue } = this.state;
-    if (previousValue !== null) {
-      this.calculateResult();
-      this.setState({ operator, previousValue: this.state.displayValue });
-    } else {
-      this.setState({ operator, previousValue: displayValue });
-    }
-    this.setState({ displayValue: '0' });
+  const handleClearClick = () => {
+    setDisplayValue('0');
+    setFirstOperand(null);
+    setOperator(null);
+    setWaitingForSecondOperand(false);
   };
 
-  render() {
-    const { displayValue } = this.state;
-    return (
-      <div className="calculator">
-        <div className="display">{displayValue}</div>
-        <div className="buttons">
-          <div className="row">
-            <button onClick={() => this.handleButtonClick('7')}>7</button>
-            <button onClick={() => this.handleButtonClick('8')}>8</button>
-            <button onClick={() => this.handleButtonClick('9')}>9</button>
-            <button onClick={() => this.setOperator('+')}>+</button>
-          </div>
-          <div className="row">
-            <button onClick={() => this.handleButtonClick('4')}>4</button>
-            <button onClick={() => this.handleButtonClick('5')}>5</button>
-            <button onClick={() => this.handleButtonClick('6')}>6</button>
-            <button onClick={() => this.setOperator('-')}>-</button>
-          </div>
-          <div className="row">
-            <button onClick={() => this.handleButtonClick('1')}>1</button>
-            <button onClick={() => this.handleButtonClick('2')}>2</button>
-            <button onClick={() => this.handleButtonClick('3')}>3</button>
-            <button onClick={() => this.setOperator('*')}>*</button>
-          </div>
-          <div className="row">
-            <button onClick={() => this.handleButtonClick('0')}>0</button>
-            <button onClick={() => this.handleButtonClick('C')}>C</button>
-            <button onClick={() => this.handleButtonClick('=')}>=</button>
-            <button onClick={() => this.setOperator('/')}>/</button>
-          </div>
+  return (
+    <div className="calculator">
+      <div className="display">{displayValue}</div>
+      <div className="buttons">
+        <div className="row">
+          <button onClick={() => handleButtonClick('7')}>7</button>
+          <button onClick={() => handleButtonClick('8')}>8</button>
+          <button onClick={() => handleButtonClick('9')}>9</button>
+          <button onClick={() => handleOperatorClick('+')}>+</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('4')}>4</button>
+          <button onClick={() => handleButtonClick('5')}>5</button>
+          <button onClick={() => handleButtonClick('6')}>6</button>
+          <button onClick={() => handleOperatorClick('-')}>-</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('1')}>1</button>
+          <button onClick={() => handleButtonClick('2')}>2</button>
+          <button onClick={() => handleButtonClick('3')}>3</button>
+          <button onClick={() => handleOperatorClick('*')}>*</button>
+        </div>
+        <div className="row">
+          <button onClick={() => handleButtonClick('0')}>0</button>
+          <button onClick={() => handleClearClick()}>C</button>
+          <button onClick={() => handleEqualClick()}>=</button>
+          <button onClick={() => handleOperatorClick('/')}>/</button>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
